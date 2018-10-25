d9dc8015b989a75c39c84915ff9cb1f4fdce2dbf
ande4694 <knudsen879@hotmail.com>
2018-10-25 12:31:14 +0200

    ready

--- src/main/java/com/example/demo/MovieRepoImpl.java
+++ src/main/java/com/example/demo/MovieRepoImpl.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -12,24 +15,46 @@ public class MovieRepoImpl implements MovieRepoInt{
 
     @Autowired
     MovieImpl movie;
-
-    @Autowired
     JdbcTemplate template;
+    private List<MovieImpl> movies = new ArrayList<>();
+    private List<MovieImpl> searched = new ArrayList<>();
+
+
 
 
     @Override
     public List<MovieImpl> getMovies() throws SQLException {
-        return null;
+
+        String sql = "SELECT * FROM movie;";
+
+        Statement stmt = null;
+
+        ResultSet rs = stmt.executeQuery(sql);
+
+        while (rs.next()){
+            int id = rs.getInt("movieid");
+            String title = rs.getString("title");
+            String production = rs.getString("production");
+            String genre = rs.getString("genre");
+
+            movies.add(new MovieImpl(id, title, production, genre));
+        }
+        rs.close();
+        stmt.close();
+        return movies;
     }
 
     @Override
     public List<MovieImpl> getSearched() {
-        return null;
+        return searched;
     }
 
     @Override
     public MovieImpl createMovie(MovieImpl movie) {
-        return null;
+        String sql = "INSERT INTO movie.movies VALUES(default, ?, ?, ?);";
+        this.template.update(sql, movie.getTitle(), movie.getYear(), movie.getGenre());
+
+        return movie;
     }
 
     @Override
